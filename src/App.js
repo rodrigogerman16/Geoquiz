93c51f6f9963fe2a1f9d63bbf1d59c5c347f1875
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-08-17 18:44:41 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -16,25 +16,26 @@ function shuffle(array) {
 
 function App() {
 
-  let countrys = ['Egipto','Corea del Sur','Reino Unido','Países Bajos','Costa Rica','Suiza','Uruguay','Croacia','Irlanda',
-  'Portugal','Paraguay','Nigeria','Suecia','Austria','Bélgica','Hungría','Bulgaria','Iraq','Israel','Luxemburgo','Tailandia',
-  'Nueva Zelanda','Dinamarca','Japón'];
-  let capitals = [['El Cairo','Lúxor','Asuán'],['Busan','Jeju','Seúl'],['Londres','Mánchester','Liverpool'],
-  ['Rótendam','La Haya','Ámsterdam'],['San José','Cartago','Alajuela'],['Ginebra','Zúrich','Berna'],
-  ['Montevideo','Punta del Este','Maldonado'],['Dubrovnik','Split','Zagreb'],['Cork','Dublín','Galway'],['Lisboa','Porto',
-  'Braga'],['Asunción','Luque','San Lorenzo'],['Lagos','Enugu','Abuya'],['Gotemburgo','Helsingborg','Estocolmo'],
-  ['Viena','Graz','Linz'],['Gante','Bruselas','Brujas'],['Budapest','Debrecen','Pécs'],['Varna','Burgas','Sofía'],
-  ['Ninaua','Basora','Bagdad'],['Beerseba','Jerusalén','Eilat'],['Luxemburgo','Ettelbruck','Dudelange'],
-  ['Ayutthaya','Chiang Mai','Bangkok'],['Wellington','Auckland','Nelson'],['Copenhague','Aarhus','Odense'],
-  ['Osaka','Kioto','Tokio']];
+  let countrys = ['Egypt','South Korea','United Kingdom','Netherlands','Costa Rica','Switzerland','Uruguay','Croatia','Ireland',
+  'Portugal','Paraguay','Nigeria','Sweden','Austria','Belgium','Hungary','Bulgaria','Iraq','Israel','Luxembourg','Thailand',
+  'New Zealand','Denmark','Japan'];
+  let capitals = [['Cairo','Luxor','Aswan'],['Busan','Jeju','Seoul'],['London','Manchester','Liverpool'],
+  ['Rotendam','Hague','Amsterdam'],['San José','Cartago','Alajuela'],['Ginebra','Zurich','Bern'],
+  ['Montevideo','Punta del Este','Maldonado'],['Dubrovnik','Split','Zagreb'],['Cork','Dublin','Galway'],['Lisbon','Porto',
+  'Braga'],['Asunción','Luque','San Lorenzo'],['Lagos','Enugu','Abuya'],['Gothenburg','Helsingborg','Stockholm'],
+  ['Vienna','Graz','Linz'],['Gante','Brussels','Brujas'],['Budapest','Debrecen','Pecs'],['Varna','Burgas','Sofía'],
+  ['Ninaua','Basora','Baghdad'],['Beersheba','Jerusalem','Eilat'],['Luxembourg','Ettelbruck','Dudelange'],
+  ['Ayutthaya','Chiang Mai','Bangkok'],['Wellington','Auckland','Nelson'],['Copenhaguen','Aarhus','Odense'],
+  ['Osaka','Kyoto','Tokyo']];
 
-  const [show, setShow] = useState(false);
   const [indice, setIndice] = useState(0);
+  const [show, setShow] = useState(true);
 
     return (
-    <div className="App">
-      
-      {!show &&
+    <div className="App"> 
+
+     {show ?
+
       <div className="Index">
         <div>
           <h2>Welcome to my Geography Quiz App</h2>
@@ -43,30 +44,31 @@ function App() {
           <h3>How to play:</h3>
           <p>Select the answer you think is the correct</p>
         </div>
-        <button onClick={() => setShow(prev => ! prev)} className="Startbtn">START</button>
-      </div>
-      }
+        <button className="Startbtn" onClick={()=>setShow(!show)}>START</button>
+      </div> 
+      
+      :
 
-      {show && 
       <div className="Game">
         <div>
-          What is the capital of {countrys[indice]}
+          <h3>What is the capital of {countrys[indice]}</h3>
+        </div>
+        <div className="Country-flag">
+          <img src={`/Flags/${countrys[indice]}.png`} alt="Country Flag" className="Country-flag"/> 
         </div>
         <div>
           <ul className='Capitals-options-container'>
             {shuffle(capitals[indice]).map(capital =>
               <button className='Capitals-options' onClick={() => {
-                if (indice < countrys.length-1) {
-                setIndice(indice + 1)
-              }}}>
-                {capital}
+                if (indice < countrys.length-1){setIndice(indice + 1)}}}>
+                  {capital}
               </button>)
             }
           </ul>
         </div>
       </div>
-      }
-
+      
+      }    
     </div>
   );
 }
